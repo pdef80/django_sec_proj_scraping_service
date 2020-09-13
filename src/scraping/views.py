2737fb0fedd3472b3e762f94c60fc1a4859b1e60
Denis <pdef8085@gmail.com>
2020-09-13 19:01:15 +0300

    made find form

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home_view(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city.lower()
+        if language:
+            _filter['language__slug'] = language.lower()
+
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
