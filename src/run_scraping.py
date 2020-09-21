b62390ad198e3346ca7684f2e60e767261344b14
Denis <pdef8085@gmail.com>
2020-09-21 19:48:12 +0300

    made async start for parser

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,9 +1,12 @@
+import asyncio
 import os
 import sys
 
 from django.contrib.auth import get_user_model
 from django.db import DatabaseError
 import codecs
+
+
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -25,13 +28,13 @@ parsers = (
             (dou, 'dou'),
             (djinny, 'djinny')
 )
+jobs, errors = [], []
 
 
 def get_settings():
     qs = User.objects.filter(send_email=True).values()
     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
     return settings_lst
-
 
 def get_urls(_settings):
     qs = Url.objects.all().values()
@@ -45,6 +48,11 @@ def get_urls(_settings):
         urls.append(tmp)
     return urls
 
+async def main(value):
+    func, url, city, language = value
+    job, err = await loop.run_in_executor(None, func, url, city, language)
+    errors.extend(err)
+    jobs.extend(job)
 
 settings = get_settings()
 url_list = get_urls(settings)
@@ -53,14 +61,22 @@ url_list = get_urls(settings)
 # language = Language.objects.filter(slug='python').first()
 
 
-jobs, errors = [], []
-for data in url_list:
-    for func, key in parsers:
-        url = data['url_data'][key]
-        j, e = func(url, city=data['city'], language=data['language'])
-        jobs += j
-        errors += e
+loop = asyncio.get_event_loop()
+tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
+             for data in url_list
+             for func, key in parsers]
+tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
 
+
+# for data in url_list:
+#     for func, key in parsers:
+#         url = data['url_data'][key]
+#         j, e = func(url, city=data['city'], language=data['language'])
+#         jobs += j
+#         errors += e
+
+loop.run_until_complete(tasks)
+loop.close()
 
 for job in jobs:
     v = Vacancy(**job)
