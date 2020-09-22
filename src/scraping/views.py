1278dc0f2d2f276a2f9de49503818f3f059c9434
Denis <pdef8085@gmail.com>
2020-09-22 23:48:57 +0300

    made pagination wth bootstrap

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -14,7 +14,8 @@ def list_view(request):
     form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    qs = []
+    page_obj = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -26,4 +27,5 @@ def list_view(request):
         paginator = Paginator(qs, 10)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-    return render(request, 'scraping/list.html', {'object_list': page_obj, 'form': form})
+        context['object_list'] = page_obj
+    return render(request, 'scraping/list.html', context)
