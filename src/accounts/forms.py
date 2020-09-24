6d1a1c7f508d8d7f919a14ed94aa9ecfc12498ea
Denis <pdef8085@gmail.com>
2020-09-25 00:14:58 +0300

    made login\logout

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -6,12 +6,12 @@ User = get_user_model()
 
 
 class UserLoginForm(forms.Form):
-    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def clean(self, *args, **kwargs):
-        email = self.cleaned_data.get('email').strip()
-        password = self.cleaned_data.get('password').strip()
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
         if email and password:
             qs = User.objects.filter(email=email)
             if not qs.exists():
